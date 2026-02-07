5b4d5ca6cc6c064c04c81d8253d48adbf8158fe3
Harsh <harshgupta0038@gmail.com>
2026-02-07 07:39:38 +0530

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+{/*  ROUTES IMPORT  */}
+import projectRoutes from "./routes/projectRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js"
+import searchRoutes from "./routes/searchRoutes.js";
+import userRoutes from "./routes/userRoutes.js"
+import teamRoutes from "./routes/teamRoutes.js"
+
+{/* CONFIGURATIONS */}
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+
+/* ROUTES*/
+app.get('/' , (req , res) =>{
+  res.send("this is home route")
+});
+
+app.use('/projects' , projectRoutes);
+app.use('/tasks' , taskRoutes);
+app.use('/search' , searchRoutes);
+app.use('/users' , userRoutes);
+app.use('/teams' , teamRoutes);
+
+
+
+{/* SERVER*/}
+const PORT = Number(process.env.PORT) || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
